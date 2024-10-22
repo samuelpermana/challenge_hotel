a95a2616a0c9dedf23198b4b1cc3fc6edb121ff3
samuelpermana <permana20samuel@gmail.com>
2024-10-22 15:36:48 +0700

    Grouped Transactions Challenge

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -12,18 +12,6 @@ class RoomsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class TransactionsSerializer(serializers.ModelSerializer):
-    guest = GuestsSerializer(read_only=True)
-    room = RoomsSerializer(read_only=True)
-    guest_id = serializers.IntegerField(write_only=True, required=True)
-    room_id = serializers.IntegerField(write_only=True, required=True)
-
     class Meta:
         model = Transactions
         fields = '__all__'
-
-    def create(self, validated_data):
-        guest_id = validated_data.pop('guest_id')
-        room_id = validated_data.pop('room_id')
-
-        transaction = Transactions.objects.create(guest_id=guest_id, room_id=room_id, **validated_data)
-        return transaction
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,39 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from .models import Guests, Rooms, Transactions
 from .serializers import GuestsSerializer, RoomsSerializer, TransactionsSerializer
+from rest_framework.response import Response
+from django.utils import timezone
+
 
 class GuestsViewSet(viewsets.ModelViewSet):
     queryset = Guests.objects.all()
     serializer_class = GuestsSerializer
+    @action(detail=False, methods=['get'], url_path="grouped-transaction")
+    def grouped_transaction(self, request):
+        guests = Guests.objects.all()
+        data = []
+
+        now = timezone.now()
+        for guest in guests:
+            transactions = Transactions.objects.filter(guest=guest)
+            grouped_transactions = {
+                "incoming": [],
+                "active": [],
+                "past": []
+            }
+            for transaction in transactions:
+                if transaction.check_in_date > now:
+                    grouped_transactions['incoming'].append(TransactionsSerializer(transaction).data)
+                elif transaction.check_in_date <= now <= transaction.check_out_date:
+                    grouped_transactions['active'].append(TransactionsSerializer(transaction).data)
+                else:
+                    grouped_transactions['past'].append(TransactionsSerializer(transaction).data)
+
+            guest_data = GuestsSerializer(guest).data
+            guest_data['transactions'] = grouped_transactions
+            data.append(guest_data)
+        return Response(data)
 
 class RoomsViewSet(viewsets.ModelViewSet):
     queryset = Rooms.objects.all()
@@ -13,4 +42,4 @@ class RoomsViewSet(viewsets.ModelViewSet):
 class TransactionsViewSet(viewsets.ModelViewSet):
     queryset = Transactions.objects.all()
     serializer_class = TransactionsSerializer
-    
+
